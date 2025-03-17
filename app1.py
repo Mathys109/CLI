0672f74e65fce08f1c88360a37f9a7ad3a4803f6
Mathys109 <mathys.precost@gmail.com>
2025-03-17 15:41:37 -0400

    Update app1.py

--- app1.py
+++ app1.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.title("Bienvenue dans mon application Streamlit")
 
 # Affichage d'un texte
-st.write("Ceci est une page fonctionnelle de Streamlit")
+st.write("Ceci nnnnnnest une page fonctionnelle de Streamlit")
 
 # Demande d'entrée à l'utilisateur
 nom = st.text_input("Quel est votre nom ?")
