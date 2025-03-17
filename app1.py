3810f3d3ddb76a2c1741159b2c834c903748e07b
Mathys109 <mathys.precost@gmail.com>
2025-03-17 15:37:48 -0400

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+# Titre de l'application
+st.title("Bienvenue dans mon application Streamlit")
+
+# Affichage d'un texte
+st.write("Ceci est une page fonctionnelle de Streamlit")
+
+# Demande d'entrée à l'utilisateur
+nom = st.text_input("Quel est votre nom ?")
+
+# Affichage du résultat
+if nom:
+    st.write(f"Bonjour, {nom} !")
